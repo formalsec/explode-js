283226bbd7c713120290ccd2859e6e6d1cbb4dfe
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-22 12:46:36 +0100

    Separate execution times

--- bin/cmd_full.ml
+++ bin/cmd_full.ml
@@ -12,8 +12,13 @@ let options filename workspace_dir time_limit =
 
 let run { filename; workspace_dir; time_limit } =
   let* _ = Bos.OS.Dir.create ~path:true ~mode:0o777 workspace_dir in
+  let graphjs_time_path = Fpath.(workspace_dir / "graphjs_time.txt") in
+  let explode_time_path = Fpath.(workspace_dir / "explode_time.txt") in
   (* 1. Run graphjs *)
-  let* (), (_, status) = Graphjs.run ~file:filename ~output:workspace_dir in
+  let graphjs_start = Unix.gettimeofday () in
+  let* status = Graphjs.run ~file:filename ~output:workspace_dir in
+  let graphjs_time = Unix.gettimeofday () -. graphjs_start in
+  let _ = Bos.OS.File.writef graphjs_time_path "%f@." graphjs_time in
   let* () =
     match status with
     | `Exited 0 -> Ok ()
@@ -27,7 +32,11 @@ let run { filename; workspace_dir; time_limit } =
         let options =
           Cmd_run.options taint_summary (Some filename) workspace_dir time_limit
         in
-        Cmd_run.main options
+        let explode_start = Unix.gettimeofday () in
+        let result = Cmd_run.main options in
+        let explode_time = Unix.gettimeofday () -. explode_start in
+        let _ = Bos.OS.File.writef explode_time_path "%f@." explode_time in
+        result
       else 0 )
 
 let main opt = match run opt with Ok n -> n | Error _err -> 1
