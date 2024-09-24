8a08bfc6447318580d4cf9bf7f336f54591e05c7
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-24 22:14:05 +0100

    POSIX timeouts

--- bench/runner.ml
+++ bench/runner.ml
@@ -1,7 +1,5 @@
 open Bos
 
-[@@@ocaml.warning "-32"]
-
 module Json = Yojson.Basic
 
 let ( let* ) = Result.bind
@@ -26,7 +24,9 @@ type 'a benchmarks =
   ; cwe1321 : 'a
   }
 
-let filename = Fpath.v "vulcan-22-78-94-471-1321-all.json"
+let log fmt k = match fmt with None -> () | Some fmt -> k (Fmt.pf fmt)
+
+let filename = Fpath.(v "datasets" / "metadata" / "vulcan-file-index.json")
 
 let config =
   let filename = Fpath.to_string filename in
@@ -73,17 +73,47 @@ let pp_time fmt
     tm_hour tm_min tm_sec
 
 let explode ~workspace_dir ~file time_limit =
-  let cmd0 = Cmd.(v "explode-js" % "full" % "--workspace" % p workspace_dir) in
-  match time_limit with
-  | None -> Cmd.(cmd0 % p file)
-  | Some timeout -> Cmd.(cmd0 % "--timeout" % string_of_int timeout % p file)
+  Cmd.(
+    v "explode-js" % "full" % "--workspace" % p workspace_dir % "--timeout"
+    % string_of_float time_limit % p file )
 
 let pp_status fmt = function
+  | `Timeout -> Fmt.pf fmt "Timeout"
   | `Exited n -> Fmt.pf fmt "Exited %a" Fmt.int n
   | `Signaled n -> Fmt.pf fmt "Signaled %a" Fmt.int n
+  | `Stopped n -> Fmt.pf fmt "Stopped %a" Fmt.int n
 
-(* FIXME: Maybe we could return the error instead of raising an exception? *)
-let run_worker timeout output benchmark =
+let wait_pid pid timeout =
+  let exception Sigchld in
+  let did_timeout = ref false in
+  let start_time = Unix.gettimeofday () in
+  let () =
+    try
+      Sys.set_signal Sys.sigchld (Signal_handle (fun _ -> raise Sigchld));
+      Unix.sleepf timeout;
+      did_timeout := true;
+      Unix.kill ~-pid Sys.sigkill;
+      Sys.set_signal Sys.sigchld Signal_default
+    with Sigchld -> ()
+  in
+  Sys.set_signal Sys.sigchld Signal_default;
+  let waited_pid, status = Unix.waitpid [] ~-pid in
+  assert (waited_pid = pid);
+  let duration = Unix.gettimeofday () -. start_time in
+  ( ( if !did_timeout then `Timeout
+      else
+        match status with
+        | WEXITED code -> `Exited code
+        | WSIGNALED code -> `Signaled code
+        | WSTOPPED code -> `Stopped code )
+  , duration )
+
+let dup2 src dst =
+  let file = Unix.openfile (Fpath.to_string dst) [ O_CREAT; O_WRONLY ] 0o666 in
+  Fun.protect ~finally:(fun () -> Unix.close file) @@ fun () ->
+  Unix.dup2 file src
+
+let fork_work fmt timeout output benchmark =
   let short_path =
     match Fpath.rem_prefix vulcan_prefix benchmark with
     | Some path -> path
@@ -94,58 +124,38 @@ let run_worker timeout output benchmark =
   | Ok _ -> ()
   | Error (`Msg err) -> Fmt.failwith "%s" err );
   let out = Fpath.(workspace_dir / "stdout") in
-  let err = OS.Cmd.err_file @@ Fpath.(workspace_dir / "stderr") in
-  let run_out =
-    OS.Cmd.run_out ~err @@ explode ~workspace_dir ~file:benchmark timeout
+  let err = Fpath.(workspace_dir / "stderr") in
+  let result, duration =
+    let pid = Unix.fork () in
+    if pid = 0 then begin
+      ExtUnix.Specific.setpgid 0 0;
+      dup2 Unix.stdout out;
+      dup2 Unix.stderr err;
+      let args = explode ~workspace_dir ~file:benchmark timeout in
+      Unix.execvp "explode-js" (Array.of_list @@ Cmd.to_list args)
+    end
+    else wait_pid pid timeout
   in
-  let status =
-    match OS.Cmd.out_file out run_out with
-    | Ok ((), (_, status)) -> status
-    | Error (`Msg _err) -> Fmt.failwith "Could not write file: %a" Fpath.pp out
-  in
-  Eio.traceln "@[<v 2>Run %a@;%a@]" Fpath.pp workspace_dir pp_status status
+  log fmt (fun m ->
+      m "@[<v 2>Run %a@;%a in %a@]@." Fpath.pp workspace_dir pp_status result
+        Fmt.float duration )
 
-let map_run_worker sw pool timeout output l =
-  List.map
-    (fun elt ->
-      Eio.Executor_pool.submit_fork ~sw pool ~weight:1.0 (fun () ->
-          run_worker timeout output elt ) )
-    l
-
-let map_wait_worker l =
-  List.map
-    (fun promise ->
-      match Eio.Promise.await promise with
-      | Ok () -> ()
-      | Error exn -> Fmt.epr "uncaught exception: %s@." (Printexc.to_string exn)
-      )
-    l
+let map_fork_work fmt time_limit output l =
+  List.map (fork_work fmt time_limit output) l
 
 let main _jobs timeout output =
-  Fmt.pr "Started at %a@." pp_time started_at;
   let* { cwe22; cwe78; cwe94; cwe471; cwe1321 } = benchmarks in
   let output = Fpath.v @@ Fmt.str "%s-%a" output pp_time started_at in
   let* _ = OS.Dir.create ~path:true ~mode:0o777 output in
-  Eio_main.run @@ fun env ->
-  let domain_mgr = Eio.Stdenv.domain_mgr env in
-  Eio.Switch.run @@ fun sw ->
-  (* Domain_count = 1 because graphjs can't be run in parallel *)
-  let domain_count = 1 in
-  let pool = Eio.Executor_pool.create ~sw ~domain_count domain_mgr in
-  let { cwe22; cwe78; cwe94; cwe471; cwe1321 } =
-    { cwe22 = map_run_worker sw pool timeout output cwe22
-    ; cwe78 = map_run_worker sw pool timeout output cwe78
-    ; cwe94 = map_run_worker sw pool timeout output cwe94
-    ; cwe471 = map_run_worker sw pool timeout output cwe471
-    ; cwe1321 = map_run_worker sw pool timeout output cwe1321
-    }
-  in
+  Out_channel.with_open_text Fpath.(to_string (output / "results")) @@ fun oc ->
+  let fmt = Some (Format.formatter_of_out_channel oc) in
+  log fmt (fun m -> m "Started at %a@." pp_time started_at);
   let results =
-    { cwe22 = map_wait_worker cwe22
-    ; cwe78 = map_wait_worker cwe78
-    ; cwe94 = map_wait_worker cwe94
-    ; cwe471 = map_wait_worker cwe471
-    ; cwe1321 = map_wait_worker cwe1321
+    { cwe22 = map_fork_work fmt timeout output cwe22
+    ; cwe78 = map_fork_work fmt timeout output cwe78
+    ; cwe94 = map_fork_work fmt timeout output cwe94
+    ; cwe471 = map_fork_work fmt timeout output cwe471
+    ; cwe1321 = map_fork_work fmt timeout output cwe1321
     }
   in
   Ok results
@@ -158,7 +168,7 @@ let cli =
   in
   let timeout =
     let doc = "Time limit per benchmark run" in
-    Arg.(value & opt (some int) None & info [ "timeout" ] ~doc)
+    Arg.(value & opt float 60. & info [ "timeout" ] ~doc)
   in
   let output =
     let doc = "Output directory to store results" in
