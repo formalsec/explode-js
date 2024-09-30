1aacae2d8864346cafd22f7d2dea9f7234ba3e35
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-30 14:04:19 +0100

    Improve marker interface

--- bench/plots/marker.ml
+++ bench/plots/marker.ml
@@ -1,20 +1,29 @@
 type t =
-  [ `Timeout
-  | `Finished
-  | `Error
-  ]
+  | Timeout
+  | Exited of int
+  | Signaled of int
+  | Stopped of int
+
+let timeout = Timeout
+
+let exited n = Exited n
+
+let signaled n = Signaled n
+
+let stopped n = Stopped n
 
 let pp fmt = function
-  | `Timeout -> Format.pp_print_string fmt "Timeout"
-  | `Finished -> Format.pp_print_string fmt "Finished"
-  | `Error -> Format.pp_print_string fmt "Error"
+  | Timeout -> Format.fprintf fmt "Timeout"
+  | Exited n -> Format.fprintf fmt "Exited %d" n
+  | Signaled n -> Format.fprintf fmt "Signaled %d" n
+  | Stopped n -> Format.fprintf fmt "Stopped %d" n
 
 let to_string v = Format.asprintf "%a" pp v
 
 let from_file file =
   In_channel.with_open_text (Fpath.to_string file) @@ fun ic ->
   let line = input_line ic in
-  if String.starts_with ~prefix:"timeout" line then `Timeout
-  else if String.starts_with ~prefix:"success" line then `Finished
-  else if String.starts_with ~prefix:"tool error" line then `Error
+  if String.starts_with ~prefix:"timeout" line then timeout
+  else if String.starts_with ~prefix:"success" line then exited 0
+  else if String.starts_with ~prefix:"tool error" line then exited 1
   else assert false
