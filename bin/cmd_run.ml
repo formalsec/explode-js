f2a188e5abe6b6b78ee527c796f28f2ff4bf3797
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-12-04 10:49:31 +0000

    Format code, update .gitignore and add .envrc

--- bin/cmd_run.ml
+++ bin/cmd_run.ml
@@ -17,8 +17,8 @@ let get_tests workspace (config : Fpath.t) (filename : Fpath.t option) =
   let output = Fpath.(to_string @@ (workspace / "symbolic_test")) in
   Run.run ~mode:0o666 ?file ~config ~output ()
 
-let run_single ~time_limit:_ ~(workspace : Fpath.t) (filename : Fpath.t)
-  original_file taint_summary : int =
+let run_single ~(workspace : Fpath.t) (filename : Fpath.t) original_file
+  taint_summary : int =
   let original_file = Option.map Fpath.to_string original_file in
   let taint_summary = Fpath.to_string taint_summary in
   let n = Cmd_symbolic.main { filename; entry_func = "main"; workspace } () in
@@ -33,14 +33,14 @@ let run_single ~time_limit:_ ~(workspace : Fpath.t) (filename : Fpath.t)
     | Ok () -> 0
   end
 
-let run_all ({ config; filename; workspace_dir; time_limit } : options) =
+let run_all ({ config; filename; workspace_dir; _ } : options) =
   let* _ = Bos.OS.Dir.create ~mode:0o777 workspace_dir in
   let* symbolic_tests = get_tests workspace_dir config filename in
   let rec loop = function
     | [] -> Ok 0
     | test :: remaning ->
       let workspace = Fpath.(workspace_dir // rem_ext (base test)) in
-      let n = run_single ~time_limit ~workspace test filename config in
+      let n = run_single ~workspace test filename config in
       if n <> 0 then Error (`Status n) else loop remaning
   in
   loop symbolic_tests
