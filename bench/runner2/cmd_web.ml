0d1891fa0ba8bfd21d96d00663de210cc5909038
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-12-10 12:11:12 +0000

    Improve Web UI
    - Update css
    - Add static dir route to tiny_httpd
    - Make results table not overflow
    - Add output html page
    - Add back nagivation buttons

--- bench/runner2/cmd_web.ml
+++ bench/runner2/cmd_web.ml
@@ -1,16 +1,14 @@
 open Jingoo
 module S = Tiny_httpd
 
-let outputs db timestamp vuln_id file _req =
-  let results = Run_result.select_db ~timestamp db in
-  let v = List.find (fun (v : Run_result.t) -> v.vuln.id = vuln_id) results in
-  let contents =
-    match file with
-    | "stdout" -> v.raw.stdout
-    | "stderr" -> v.raw.stderr
-    | _ -> Jg_template.from_file Templates.not_found
-  in
-  S.Response.make_string (Ok contents)
+let ( let* ) = Result.bind
+
+let root db _req =
+  let open Jg_types in
+  let runs = Run_metadata.select_db db |> List.map Run_metadata.to_jg in
+  let models = [ ("title", Tstr "Explode-js"); ("runs", Tlist runs) ] in
+  let page = Jg_template.from_file ~models Share.Templates.index in
+  S.Response.make_string (Ok page)
 
 let results db timestamp _req =
   let open Jg_types in
@@ -20,15 +18,30 @@ let results db timestamp _req =
     |> List.map Run_result.to_jg
   in
   let models = [ ("timestamp", Tint timestamp); ("results", Tlist results) ] in
-  let page = Jg_template.from_file ~models Templates.results in
+  let page = Jg_template.from_file ~models Share.Templates.results in
   S.Response.make_string (Ok page)
 
-let root db _req =
-  let open Jg_types in
-  let runs = Run_metadata.select_db db |> List.map Run_metadata.to_jg in
-  let models = [ ("title", Tstr "Explode-js"); ("runs", Tlist runs) ] in
-  let page = Jg_template.from_file ~models Templates.index in
-  S.Response.make_string (Ok page)
+let outputs db timestamp vuln_id file _req =
+  let results = Run_result.select_db ~timestamp db in
+  let v = List.find (fun (v : Run_result.t) -> v.vuln.id = vuln_id) results in
+  let contents =
+    match file with
+    | "stdout" -> Ok v.raw.stdout
+    | "stderr" -> Ok v.raw.stderr
+    | "report" -> Ok v.report
+    | _ -> Error (404, Jg_template.from_file Share.Templates.not_found)
+  in
+  S.Response.make_string
+  @@
+  let* contents in
+  let models =
+    Jg_types.
+      [ ("title", Tstr file)
+      ; ("timestamp", Tint timestamp)
+      ; ("output", Tstr contents)
+      ]
+  in
+  Ok (Jg_template.from_file ~models Share.Templates.output)
 
 let main addr port db_path =
   Db.with_db ~mode:`READONLY (Fpath.to_string db_path) @@ fun db ->
@@ -42,6 +55,10 @@ let main addr port db_path =
   S.add_route_handler server
     S.Route.(exact "results" @/ int @/ int @/ string @/ return)
     (outputs db);
+  (* Dirs *)
+  let config = S.Dir.default_config () in
+  let dir = List.hd Share.Static.root in
+  S.Dir.add_dir_path ~config ~dir ~prefix:"static" server;
 
   Fmt.epr "listening on http://%s:%d@." (S.addr server) (S.port server);
   match S.run server with Ok () -> Ok () | Error e -> Error (`Exn e)
