458c39de881483c7c5b81a4834f5105f282a677a
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-03-05 16:34:47 +0000

    Add dot token and more parser tests

--- test/parser/test_parser.ml
+++ test/parser/test_parser.ml
@@ -1,10 +1,37 @@
 open Auto_complete
 
+let ( let* ) = Result.bind
+
+let ( let+ ) v f = Result.map f v
+
+let nl fmt () = Fmt.pf fmt "@\n"
+
+let test_empty () =
+  let+ stmts = Parse.from_string "" in
+  Fmt.pr "%a@." (Fmt.list ~sep:nl Fmt.string) stmts
+
+let test_valid () =
+  let+ stmts = Parse.from_string "var x = 0; var y = 1" in
+  Fmt.pr "%a@." (Fmt.list ~sep:nl Fmt.string) stmts
+
+let test_invalid_assignment () =
+  let+ stmts = Parse.from_string "var x = " in
+  Fmt.pr "%a@." (Fmt.list ~sep:nl Fmt.string) stmts
+
+let test_invalid_expression () =
+  let+ stmts = Parse.from_string "cl." in
+  Fmt.pr "%a@." (Fmt.list ~sep:nl Fmt.string) stmts
+
+let _test_invalid_expression2 () =
+  let+ stmts = Parse.from_string "(" in
+  Fmt.pr "%a@." (Fmt.list ~sep:nl Fmt.string) stmts
+
+let result =
+  let* () = test_empty () in
+  let* () = test_valid () in
+  let* () = test_invalid_assignment () in
+  let+ () = test_invalid_expression () in
+  ()
+
 let () =
-  let result = Parse.from_string "var x = 0; var y = 1; var z = " in
-  match result with
-  | Ok stmts ->
-    Fmt.pr "%a@."
-      (Fmt.list ~sep:(fun fmt () -> Fmt.pf fmt "@\n") Fmt.string)
-      stmts
-  | Error msg -> Fmt.epr "%s" msg
+  match result with Ok () -> () | Error err -> Fmt.failwith "error: %s" err
