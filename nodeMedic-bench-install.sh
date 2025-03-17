f408a15cc5ffdea5bb706d41c0694219c0303881
frediramos <32137225+frediramos@users.noreply.github.com>
2025-03-17 23:10:01 +0000

    Remove comment in nodeMedic-bench-install.sh

--- nodeMedic-bench-install.sh
+++ nodeMedic-bench-install.sh
@@ -1,2 +1,2 @@
-# cd bench/NodeMedic && docker build --platform=linux/amd64 -t nodemedic-fine:latest .
+cd bench/NodeMedic && docker build --platform=linux/amd64 -t nodemedic-fine:latest .
 python3 -m pip install pandas~=2.2.3 tabulate~=0.9.0
