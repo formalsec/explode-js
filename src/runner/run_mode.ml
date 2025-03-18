2ccf9d751a92f5f9ccfcab518404a56383df4124
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-03-18 09:15:48 +0000

    Add run-no-vis mode to runner

--- src/runner/run_mode.ml
+++ src/runner/run_mode.ml
@@ -1,14 +1,24 @@
+type run_type =
+  | Expected
+  | No_vis
+
+let dispatch = function
+  | Expected -> "expected_output.json"
+  | No_vis -> "no-vis.json"
+
 type t =
   | Full
-  | Run
+  | Run of run_type
 
 let pp fmt = function
   | Full -> Fmt.string fmt "full"
-  | Run -> Fmt.string fmt "run"
+  | Run Expected  -> Fmt.string fmt "run"
+  | Run No_vis -> Fmt.string fmt "run-no-vis"
 
 let of_string = function
   | "full" -> Ok Full
-  | "run" -> Ok Run
+  | "run" -> Ok (Run Expected)
+  | "run-no-vis" -> Ok (Run No_vis)
   | s -> Error (`Msg (Fmt.str "unexpected run mode: %s" s))
 
 let conv = Cmdliner.Arg.conv (of_string, pp)
