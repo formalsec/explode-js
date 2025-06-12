5c9522e83d06206b7fa5bbad738753a98b0ec594
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-06-13 00:11:03 +0100

    Add api to export outcomes and sinks from the runner's web UI

--- src/runner/cmd_web.ml
+++ src/runner/cmd_web.ml
@@ -46,14 +46,64 @@ let outputs db timestamp vuln_id file _req =
   in
   Ok (Jg_template.from_file ~models Share.Templates.output)
 
-let to_csv db timestamp _req =
+let export_csv db timestamp _req =
   let results =
     Run_result.select_db ~timestamp db
     |> List.sort (fun (r1 : Run_result.t) r2 -> compare r1.vuln.id r2.vuln.id)
   in
   S.Response.make_string (Ok (Run_result.to_csv_string_short results))
 
-let main addr port db_path =
+module Sset = Set.Make (String)
+
+let export_outcomes db timestamp _req =
+  (* FIXME: This is pretty hackish thing I made while trying not to fall asleep.
+     Review it with fresh eyes *)
+  let module Json = Yojson.Basic in
+  let module Util = Json.Util in
+  let results = Run_result.select_db ~timestamp db in
+  let results =
+    List.map
+      (fun result ->
+        let Run_result.{ pkg; vuln; report; control_path; exploit; _ } =
+          result
+        in
+        match report with
+        | "" ->
+          [ Fmt.str "%s|%s|%a|%B|%B|--|--|--|--" pkg.package pkg.version
+              Vulnerability.pp_csv vuln control_path exploit
+          ]
+        | _ -> begin
+          let prefix =
+            Fmt.str "%s|%s|%a|%B|%B" pkg.package pkg.version
+              Vulnerability.pp_csv vuln control_path exploit
+          in
+          let reports = Json.from_string report |> Util.to_list in
+          let res =
+            List.concat_map
+              (fun json ->
+                let filename = Util.member "filename" json |> Util.to_string in
+                let failures = Util.member "failures" json |> Util.to_list in
+                List.map
+                  (fun failure ->
+                    let type_ = Util.member "type" failure |> Util.to_string in
+                    let sink =
+                      String.escaped
+                        (Util.member "sink" failure |> Util.to_string)
+                    in
+                    Fmt.str "%s|%s|%s|%s" prefix filename type_ sink )
+                  failures )
+              reports
+          in
+          res
+        end )
+      results
+  in
+  let header =
+    "package|version|id|cwe|filename|control-path|exploit|symbolic-test|outcome|sink"
+  in
+  let results = header :: List.flatten results in
+  S.Response.make_string (Ok (String.concat "\n" results))
+
 let main ~address ~port ~db_path =
   Db.with_db ~mode:`READONLY (Fpath.to_string db_path) @@ fun db ->
   let server = S.create ~addr:address ~port () in
@@ -68,7 +118,11 @@ let main ~address ~port ~db_path =
     (outputs db);
   S.add_route_handler server
     S.Route.(exact "results" @/ int @/ exact "csv" @/ return)
-    (to_csv db);
+    (export_csv db);
+  S.add_route_handler server
+    S.Route.(exact "results" @/ int @/ exact "outcomes" @/ return)
+    (export_outcomes db);
+
   (* Dirs *)
   let config = S.Dir.default_config () in
   let dir = List.hd Share.Static.root in
