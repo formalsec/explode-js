111c1a5af8189f6f63e2600809699720d489c806
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-06-27 13:10:29 +0100

    Manage ECMA-SL through an opam pin instead of a submodule
    Easier to install and so on

--- src/core/sym_exec.ml
+++ src/core/sym_exec.ml
@@ -46,6 +46,7 @@ let run_file ~deterministic ~lazy_values ~workspace_dir input_file =
     try
       run ~lazy_values ~timeout:30 ~print_return_value:false
         ~no_stop_at_failure:false
+        ~solver_type:Smtml.Solver_type.Z3_solver
         ~callback_out:(fun _ _ -> ())
         ~callback_err:(fun thread ty ->
           let solver = Choice.solver thread in
