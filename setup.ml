272f1ab0c84329244db6fa604333c25e965b705c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-09-06 12:04:39 +0100

    refactor(setup): pass strings instead of commands to execute

--- setup.ml
+++ setup.ml
@@ -17,41 +17,42 @@ let with_dir fpath f =
   Sys.chdir fpath;
   Fun.protect ~finally:(fun () -> Sys.chdir cwd) f
 
-let on_fail res msg = if res <> 0 then Format.ksprintf failwith "Error: %s" msg
+let execute f_name cmd err_msg =
+  let res = Sys.command cmd in
+  if res <> 0 then Format.ksprintf failwith "%s: %s" f_name err_msg
 
 let opam_install pkg =
-  Format.ksprintf Sys.command "opam install %s -y --confirm-level=unsafe-yes"
-    pkg
+  Format.sprintf "opam install %s -y --confirm-level=unsafe-yes" pkg
 
-let opam_exec rest = Format.ksprintf Sys.command "opam exec -- %s" rest
+let opam_exec rest = Format.sprintf "opam exec -- %s" rest
 
 let setup_graphjs () =
+  let execute = execute "setup_graphjs" in
   Format.printf "Installing graphjs ...@.";
   with_dir "vendor/graphjs" @@ fun () ->
-  on_fail
-    (Sys.command "pip install -r ./requirements.txt")
+  execute "pip install -r ./requirements.txt"
     "Could not install graphjs's python requirements";
   with_dir "parser" @@ fun () ->
-  on_fail
-    (Sys.command "npm install")
-    "Could not install graphjs's normalizer dependencies";
-  on_fail (Sys.command "npm exec tsc") "Could not compile graphjs's normalizer"
+  execute "npm install" "Could not install graphjs's normalizer dependencies";
+  execute "npm exec tsc" "Could not compile graphjs's normalizer"
 
 let setup_z3 () =
+  let execute = execute "setup_z3" in
   Format.printf "Installing Z3 ...@.";
-  on_fail (opam_install "z3") "Could not install z3 through opam!"
+  execute (opam_install "z3") "Could not install z3 through opam!"
 
 let setup_explodejs () =
+  setup_z3 ();
+  let execute = execute "setup_explodejs" in
   Format.printf "Installing Explode-js ...@.";
-  on_fail
+  execute
     (opam_install ". --deps-only --with-test --with-doc")
     "Could not install Explode-js's dependencies";
-  on_fail
+  execute
     (opam_exec "dune build @install --profile release")
     "Could not build Explode-js!";
-  on_fail (opam_exec "dune install") "Could not install Explode-js!"
+  execute (opam_exec "dune install") "Could not install Explode-js!"
 
 let () =
   if !skip_python then () else setup_graphjs ();
-  setup_z3 ();
   setup_explodejs ()
