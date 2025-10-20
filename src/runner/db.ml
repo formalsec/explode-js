778ec3532822948d1e7c5dc9830160eaea3e8a75
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-10-20 10:50:22 +0100

    Remove benchmark runner from repo

--- src/runner/db.ml
+++
@@ -1,9 +0,0 @@
-include Sqlite3_utils
-
-let to_result = function
-  | Ok v -> Ok v
-  | Error rc -> Error (`Sqlite3 rc)
-
-let unwrap_db = function
-  | Ok v -> v
-  | Error rc -> Fmt.failwith "uncaught sqlite3 error: %s" (Rc.to_string rc)
