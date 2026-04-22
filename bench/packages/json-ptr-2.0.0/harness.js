ec8b855d886034358cc6f178e42378d1b25b403c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-04-22 12:10:14 +0100

    [bench] continue adding harnesses and metadata

---
+++ bench/packages/json-ptr-2.0.0/harness.js
@@ -0,0 +1,8 @@
+const { JsonPointer } = require("json-ptr");
+const esl_symbolic = require("esl_symbolic");
+var payload = esl_symbolic.string("payload");
+//`/aaa'])) !== 'undefined') {return it;}; ${customSpawn}; spawnSync("touch",["json-ptr"]); if(((['a`
+JsonPointer.get(
+  {},
+  payload
+);
