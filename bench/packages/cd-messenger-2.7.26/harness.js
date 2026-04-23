b709501b3c1da94cdfa812565bb8a6ac05c70ad9
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-04-23 11:27:19 +0100

    [bench] add cd-messenger harnesses and metadata

---
+++ bench/packages/cd-messenger-2.7.26/harness.js
@@ -0,0 +1,6 @@
+const a = require("cd-messenger");
+const esl_symbolic = require("esl_symbolic");
+let payload = esl_symbolic.string("payload");
+//"red(); require('fs').writeFileSync(' ./cd-messenger ',``); //"
+a.line(payload);
+
