cc724e75b0d2cc41fe6add6502a1cf0189e847c1
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2026-05-01 15:21:21 +0100

    roar-pidusage-1.1.6: harness complete and verified

---
+++ bench/packages/roar-pidusage-1.1.6/harness.js
@@ -0,0 +1,11 @@
+const esl = require('esl_symbolic');
+const os = require("os");
+os.platform = () => {
+  return "freebsd";
+};
+const roar_pidusage = require("roar-pidusage");
+
+try {
+  roar_pidusage.stat(esl.string('payload'), function () {});
+} catch (e) {}
+
