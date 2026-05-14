1259b0b15ea36768613a8180a97a52c36fa508eb
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-14 21:46:43 +0100

    thenify: complete and verified

--- bench/packages/thenify-3.3.0/harness.js
+++ bench/packages/thenify-3.3.0/harness.js
@@ -1,5 +1,7 @@
-const thenify = require("thenify");
 const esl = require("esl_symbolic");
+require.cache[require.resolve("any-promise")] = { exports: Promise };
+
+const thenify = require("thenify");
 
 function cur() {}
 Object.defineProperty(cur, "name", {
