ca90f72f9142f0e972e2a6e7fc990a0f66074611
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-15 11:45:50 +0100

    ts-process-promises: complete and verified

---
+++ bench/packages/ts-process-promises-1.0.2/harness.js
@@ -0,0 +1,3 @@
+const esl = require("esl_symbolic");
+const a = require("ts-process-promises");
+a.exec(esl.string("payload"), {})
---
+++ bench/packages/ts-process-promises-1.0.2/poc.js
@@ -0,0 +1,2 @@
+const a = require("ts-process-promises");
+a.exec("touch ts-process-promises", {})
