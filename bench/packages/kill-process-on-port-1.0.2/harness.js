8dc9bfe03990cabde2b17bf7b806489bdc7d0077
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-24 20:11:59 +0100

    kill-process-on-port: harness written and verified

--- bench/packages/kill-process-on-port-1.0.2/harness.js
+++ bench/packages/kill-process-on-port-1.0.2/harness.js
@@ -1,3 +1,6 @@
 const esl = require("esl_symbolic");
+require.cache[require.resolve("portfinder")] = { exports: esl.lazy_object() };
+require.cache[require.resolve("inquirer")] = { exports: esl.lazy_object() };
+
 const lib = require('kill-process-on-port');
-lib.killProcessOnPort(esl.string("port"));
+lib.killProcessOnPort(esl.string("port"), false);
