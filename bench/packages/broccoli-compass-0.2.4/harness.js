1bd57d8912bd3f9e9b548abc88e34777c7116682
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-24 19:32:54 +0100

    broccoli-compass: harness complete and verified

--- bench/packages/broccoli-compass-0.2.4/harness.js
+++ bench/packages/broccoli-compass-0.2.4/harness.js
@@ -1,4 +1,6 @@
 var esl = require("esl_symbolic");
+require.cache[require.resolve("quick-temp")] = { exports: esl.lazy_object() };
+
 var compileSass = require('broccoli-compass');
 compileSass({}, {
   files: [esl.string("payload")]
