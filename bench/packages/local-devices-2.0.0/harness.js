833276b5169ff2d810b48ecdbd6d005ed4921fde
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-24 23:16:10 +0100

    local-devices: harness written and verified

--- bench/packages/local-devices-2.0.0/harness.js
+++ bench/packages/local-devices-2.0.0/harness.js
@@ -1,4 +1,22 @@
 const esl = require("esl_symbolic");
+require.cache[require.resolve("mz/child_process")] = { exports: require("child_process") };
+
+class Socket {
+  constructor() {
+    console.log("Socket stub");
+  }
+  setTimeout(a, b) { }
+  connect(a, b, c) { }
+  once(a, cb) {
+    // call cb to resolve promise
+    cb()
+  }
+  destroy() { }
+}
+
+require.cache["net"] = { exports: { Socket: Socket } };
+
 const find = require("local-devices");
 let userInput = esl.string("userInput");
+esl.assume(userInput != "");
 find(userInput);
