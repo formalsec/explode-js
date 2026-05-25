c631220a580070f4456bb7f8ccf3a1e9ddbdbc3e
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-25 16:24:16 +0100

    [bench] format package.json

--- bench/packages/kill-port-process-2.1.0/harness.js
+++ bench/packages/kill-port-process-2.1.0/harness.js
@@ -1,3 +1,5 @@
-const killPortProcess = require("kill-port-process");
 const esl = require("esl_symbolic");
+require.cache[require.resolve("pid-from-port")] = { exports: esl.lazy_object() };
+
+const killPortProcess = require("kill-port-process");
 killPortProcess.killPortProcess(esl.string("payload"))
--- bench/packages/lifion-verify-deps-1.1.0/harness.js
+++
@@ -1,4 +0,0 @@
-const esl = require("esl_symbolic");
-require.cache[require.resolve("chalk")] = { exports: esl.lazy_object() };
-const verifyDeps = require('lifion-verify-deps');
-verifyDeps({ dir: "../", logger: console });
