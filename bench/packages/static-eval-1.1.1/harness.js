f7c744eadc2464ee28ad703686564877ae9a8539
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-26 19:45:58 +0100

    [bench] fix harnesses

--- bench/packages/static-eval-1.1.1/harness.js
+++
@@ -1,7 +0,0 @@
-const evaluate = require("static-eval");
-const parse = require("esprima").parse;
-const esl_symbolic = require("esl_symbolic");
-var payload = esl_symbolic.string("payload");
-let ast = parse(payload).body[0].expression;
-let res = evaluate(ast, {});
-//"(function(){1 + (Object.prototype.polluted = `yes`)}())"
