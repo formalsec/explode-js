2e6222c5f8200f967e1fb8cd02260733a6b4e081
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-27 15:17:42 +0100

    query-mysql: clean up harness

--- bench/packages/query-mysql-0.0.2/harness.js
+++ bench/packages/query-mysql-0.0.2/harness.js
@@ -1,33 +1,4 @@
 const esl = require('esl_symbolic');
-
-class Mysql {
-  constructor(options) {
-    console.log("mysql constructor");
-  }
-
-  connect(cb) {
-    console.log("mysql connect");
-    // always return a valid connection
-    let error = null;
-    cb(error)
-  }
-
-  query(queryStr, cb) {
-    console.log("mysql query", queryStr);
-    esl.sinkCall("sql-injection", "query", queryStr);
-  }
-
-  end() { }
-}
-
-require.cache[require.resolve("mysql")] = {
-  exports: {
-    createConnection: function (options) {
-      return new Mysql(options);
-    }
-  }
-}
-
 const queryMysql = require("query-mysql");
 
 function createClient() {
@@ -40,7 +11,6 @@ function createClient() {
   function fetchById(table, id, name_id) {
     return new Promise((resolve, reject) => {
       const qm = newQm();
-      console.log("Here")
       qm.base.fetchById(table, id, name_id, (status, rows) => {
         if (status !== "success") {
           return reject(new Error(`query-mysql status: ${status}`));
